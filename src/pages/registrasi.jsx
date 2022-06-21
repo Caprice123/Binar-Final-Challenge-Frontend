52d955746d137700c2be154ae0c1b3bab9a5ad33
Sri Handayani <srihandayani61724@gmail.com>
2022-06-21 16:58:55 +0700

    login & register

--- src/pages/registrasi.jsx
+++ src/pages/registrasi.jsx
@@ -1,42 +1,51 @@
 import React from 'react';
 
-import '../assets/css/auth.style.css'
+import styles from '../assets/css/auth.module.css'
 import imageCover from '../assets/images/cover.png'
+import ActiveButton from '../components/ActionButton';
+import Input from '../components/Input';
 
 const Registrasi = () => {
     return (
         <div>
-            <div className="page-login vh-100">
+            <div className={styles.page_auth + " vh-100"}>
                 <div className="container-fluid h-100">
                     <div className="row align-items-center h-100">
-                        <div className="col-md-6 d-none d-md-block login__left">
-                            <div className="login-cover">
-                                <div className="brand">
-                                    <p>Second</p>
-                                    <p>Hand.</p>
+                        <div className={styles.auth_left + " col-md-6 d-none d-md-block"}>
+                            <div className={styles.auth_cover}>
+                                <div className={styles.brand}>
+                                    <p className={styles.text_brand}>Second</p>
+                                    <p className={styles.text_brand}>Hand.</p>
                                 </div>
                                 <img src={imageCover} alt="" />
                             </div>
                         </div>
-                        <div className="col-md-6 col-12 login__right">
-                            <div className="login__form-wrapper">
+                        <div className={styles.auth_right + " col-md-6 col-12"}>
+                            <div className={styles.auth_form_wrapper}>
                                 <h3 className="title fw-bold">Daftar</h3>
                                 <form action="">
-                                <div class="mb-3">
-                                        <label for="email" class="form-label">Nama </label>
-                                        <input type="email" class="form-control rounded-pill" id="nama" />
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="email" class="form-label">Email </label>
-                                        <input type="email" class="form-control rounded-pill" id="email" />
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="password" class="form-label">Password</label>
-                                        <input type="password" class="form-control rounded-pill" id="password" />
-                                    </div>
-                                    <button className="btn btn-primary d-block w-100 rounded-pill">Daftar</button>
+                                    <Input 
+                                        type="text"
+                                        text="Nama"
+                                        placeholder="Masukan nama anda"
+                                    />
+                                    <Input 
+                                        type="email"
+                                        text="Email"
+                                        placeholder="handayani@gmail.com"
+                                    />
+                                    <Input 
+                                        type="password"
+                                        text="Password"
+                                        placeholder="Masukan password anda"
+                                    />
+                                    <ActiveButton 
+                                        width="100%"
+                                        color="#7126B5"
+                                        text="Masuk"
+                                    />
                                 </form>
-                                <div className="footer">
+                                <div className={styles.footer}>
                                     <p className='text-center mt-3'>Belum punya akun? <a href=""class="text-purple">Daftar disini</a></p>
                                 </div>
                             </div>
