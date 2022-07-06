7fc6af6e8cc3a4d2965e418c38e30e9085231f68
Kelvin Cendra <106051926+Kelvin-Cendra@users.noreply.github.com>
2022-07-06 20:15:28 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
 
- import 'bootstrap/dist/css/bootstrap.min.css';
- import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
