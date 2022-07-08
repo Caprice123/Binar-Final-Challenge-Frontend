54428a48d01d45ec3a670d701c853d2ce84d66ad
Kelvin-Cendra <kelvin.cendra@fazzfinancial.com>
2022-07-08 19:58:35 +0700

    fix: solving position action button

--- src/components/ActionButton/index.js
+++ src/components/ActionButton/index.js
@@ -6,7 +6,7 @@ import { Wrapper } from './ActionButton.styles'
 const ActionButton = ({ width, color, icon, text, onClick, ...additionalProps }) => {
     return (
         <Wrapper type="button" 
-                className="btn d-flex align-items-center btn-primary"
+                className="btn d-flex align-items-center justify-content-center btn-primary"
                 width={width}
                 color={color} 
                 onClick={onClick}
