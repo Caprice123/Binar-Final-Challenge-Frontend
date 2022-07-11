f9f63488701b7d664478f2101196f0207d265a71
Kelvin-Cendra <kelvin.cendra@fazzfinancial.com>
2022-07-11 23:17:18 +0700

    update config base url api

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
-    // baseURL: "https://secondhand-2-binar-final.herokuapp.com/",
+    baseURL: process.env.REACT_APP_BASE_URL,
 })
 
 export default api
