e8b220b156ea77dddcf98535800ffa83ba0052e0
Kelvin-Cendra <kelvin.cendra@fazzfinancial.com>
2022-07-12 23:28:11 +0700

    removing bids and product slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-// import bidSlice from './bids'
-// import productSlice from './product'
 import statusSlice from './status'
 import userSlice from './user'
 
 
 const store = configureStore({
     reducer: {
-        // bids: bidSlice.reducer,
-        // product: productSlice.reducer,
         user: userSlice.reducer,
         status: statusSlice.reducer,
     }
